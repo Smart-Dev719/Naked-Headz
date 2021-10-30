1076a7861a7a43eaab718a661fe9032f0e4cd4f2
Blockchain-Dev719 <blockchaindev719@gmail.com>
2021-10-30 12:54:57 -0400

    discord url change

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,30 +1,47 @@
-import React from 'react';
-import '../App.css'
+import React from "react";
+import "../App.css";
 
 class Navbar extends React.Component {
     render() {
         return (
-          <div className="position">
+            <div className="position">
                 <nav className="social-icons">
-                    <a href="https://medium.com/@nakedheadz/naked-headz-faq-8d0ab624af9a" 
-                    title="Medium" target="_blank" rel="noreferrer" >
-                    <i class='fab fa-medium'></i>
-                    </a> 
-                    <a href="https://twitter.com/NakedHeadz" title="Twitter" 
-                    target="_blank" rel="noreferrer" >
-                    <i class='fab fa-twitter-square'></i>
-                    </a> 
-                    <a href="https://www.instagram.com/naked_headz/" title ="Instagram" target="_blank" rel="noreferrer">
-                    <i class='fab fa-instagram'></i>
-                    </a> 
-                    <a href="https://discord.gg/3eP88vPk" title="Discord" 
-                    target="_blank" rel="noreferrer" >
-                    <i class='fab fa-discord'></i>
+                    <a
+                        href="https://medium.com/@nakedheadz/naked-headz-faq-8d0ab624af9a"
+                        title="Medium"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i class="fab fa-medium"></i>
+                    </a>
+                    <a
+                        href="https://twitter.com/NakedHeadz"
+                        title="Twitter"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i class="fab fa-twitter-square"></i>
+                    </a>
+                    <a
+                        href="https://www.instagram.com/naked_headz/"
+                        title="Instagram"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a
+                        href="https://discord.gg/bnD9W9WZ"
+                        title="Discord"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i class="fab fa-discord"></i>
                     </a>
                 </nav>
-          </div>
+            </div>
         );
     }
- }
+}
 
- export default Navbar;
+export default Navbar;
